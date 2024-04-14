bb57b52fd4355f3881d5833e62a73786107ec16a
simon <ssuchanovsky@student.umb.sk>
2024-04-14 22:21:05 +0200

    pridanie tlacidla, ktore vrati id treningoveho planu

--- src/app/training-plan/training-plan.component.ts
+++ src/app/training-plan/training-plan.component.ts
@@ -1,4 +1,4 @@
-import {Component, NgModule} from '@angular/core';
+import {Component, NgModule, OnInit} from '@angular/core';
 import {RouterLink, RouterOutlet} from "@angular/router";
 import {MatButton} from '@angular/material/button';
 import {MatTooltip} from '@angular/material/tooltip';
@@ -8,6 +8,7 @@ import {MatToolbar} from "@angular/material/toolbar";
 import {DemoService} from "../services/demo.service";
 import {MatTable} from "@angular/material/table";
 import {MatTableModule} from '@angular/material/table';
+import {MatTabHeader} from "@angular/material/tabs";
 @Component({
   selector: 'app-training-plan',
   standalone: true,
@@ -22,20 +23,34 @@ import {MatTableModule} from '@angular/material/table';
     RouterOutlet,
     MatMenuTrigger,
     MatTable,
-    MatTableModule
+    MatTableModule,
+    MatTabHeader
   ],
   templateUrl: './training-plan.component.html',
   styleUrl: './training-plan.component.css'
 })
 
-export class TrainingPlanComponent {
+export class TrainingPlanComponent implements OnInit{
   data :any;
+  data2 :any;
   constructor(private demoService : DemoService) { }
+  ngOnInit() {
+    this.loadTrainingPlans();
+  }
 
   displayedColumns: string[] = ['nazovCviku', 'narocnostCviku', 'popisCviku'];
-
+  displayedColumns2: string[] = ['planId','nazov', 'popis'];
     loadData(): void {
       this.data = this.demoService.getCvicenieList();
       console.log(this.data);
+      console.log("tadaaaaaa");
     }
+    loadTrainingPlans() : void {
+      this.data2 = this.demoService.getTreningovePlanyList();
+    }
+  pokus(planId: number): void {
+    console.log(planId);
+  }
 }
+
+
